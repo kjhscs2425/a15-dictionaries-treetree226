1d4e0b2b493e2fd2ca4097145d4822ef4b09f691
treetree226 <tthai2025@kehillahstudent.org>
2025-03-17 09:12:35 -0700

    done

--- main.py
+++ main.py
@@ -1,7 +1,16 @@
-text = """
+texts = """
 Everyone counts.
 Everyone has access to great learning.
 Every person's unique talents are valued equally.
 Everyone takes responsibility for themselves and their community."""
 
 # Make a dictionary where the keys are characters and the values are the number of times that character appears in the `text` print the dicitonary
+
+text_counts = {}
+for text in texts:
+    if text in text_counts:
+        text_counts[text]+= 1
+    else:
+        text_counts[text] = 1
+
+print(text_counts)
